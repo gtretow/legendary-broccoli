fe605e05f41b7e795209511b5a095a6d8fdf0f3f
gtretow <guilherme.tretow@gmail.com>
2024-10-13 16:25:16 -0300

    minor fix

--- front-end/src/js/config.js
+++ front-end/src/js/config.js
@@ -2,6 +2,5 @@ export const config = {
   apiUrl:
     window.location.hostname === "localhost"
       ? "http://localhost:5000/api"
-      : "https://api.minhaapp.com",
-  /*  apiUrl: "http://localhost:5000/api", */
+      : "https://98.80.79.255:5000/api",
 };
