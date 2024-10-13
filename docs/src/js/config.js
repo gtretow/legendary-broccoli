1d7d40c3a8384acc1abf96dd192c6c018c5e9edb
gtretow <guilherme.tretow@gmail.com>
2024-10-13 16:36:42 -0300

    fix

--- docs/src/js/config.js
+++ docs/src/js/config.js
@@ -1,6 +1,7 @@
 export const config = {
-  apiUrl:
-    window.location.hostname === "localhost"
-      ? "http://localhost:5000/api"
-      : "https://api.minhaapp.com/",
+  apiUrl: "https://api.minhaapp.com/",
 };
+
+/* window.location.hostname === "localhost"
+? "http://localhost:5000/api"
+: "https://api.minhaapp.com/", */
